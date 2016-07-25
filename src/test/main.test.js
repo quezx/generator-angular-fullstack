836e3149eccc54b4e4327776854cf644623f13a9
Andrew Koroluk <koroluka@gmail.com>
2016-07-24 22:18:17 -0400

    test

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -131,12 +131,12 @@ describe('angular-fullstack:app', function() {
     });
 
     if(!process.env.SKIP_E2E) {
-      it.skip('should run e2e tests successfully', function() {
+      it('should run e2e tests successfully', function() {
         this.retries(2);
         return runCmd('gulp test:e2e').should.be.fulfilled();
       });
 
-      it.skip('should run e2e tests successfully for production app', function() {
+      it('should run e2e tests successfully for production app', function() {
         this.retries(2);
         return runCmd('gulp test:e2e:prod').should.be.fulfilled();
       });
